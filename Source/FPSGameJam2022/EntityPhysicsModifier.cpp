fdef0451b3a2366ef43efd511431ab09eaa2b897
Spencer Gellner <spencergellner@gmail.com>
2022-12-11 11:17:29 -0800

    Color Changes

---
+++ Source/FPSGameJam2022/EntityPhysicsModifier.cpp
@@ -0,0 +1,117 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EntityPhysicsModifier.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values for this component's properties
+UEntityPhysicsModifier::UEntityPhysicsModifier()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	LowGravityScale = 0.3f;
+	FrictionCoef = 2;
+	MaxAcceleration = 1000;
+
+	MixedColor = EColorLights::CL_Nothing;
+	// ...
+}
+
+
+// Called when the game starts
+void UEntityPhysicsModifier::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MovementComponent = GetOwner()->FindComponentByClass<UCharacterMovementComponent>();
+	// ...
+	
+}
+
+
+// Called every frame
+void UEntityPhysicsModifier::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	// ...
+}
+
+void UEntityPhysicsModifier::SetColor(EColorLights NewColor)
+{
+	if (Color1 == EColorLights::CL_Nothing)
+	{
+		Color1 = NewColor;
+	}
+	else if (Color2 == EColorLights::CL_Nothing)
+	{
+		Color2 = NewColor;
+	}
+	MixedColor = GetMixedColorStage();
+}
+
+void UEntityPhysicsModifier::RemoveColor(EColorLights ColorToRemove)
+{
+	if (Color1 == ColorToRemove)
+	{
+		Color1 = EColorLights::CL_Nothing;
+	}
+	else if (Color2 == ColorToRemove)
+	{
+		Color2 = EColorLights::CL_Nothing;
+	}
+	MixedColor = GetMixedColorStage();
+}
+
+EColorLights UEntityPhysicsModifier::GetMixedColorStage()
+{
+	bool isCyanA = Color1 == EColorLights::CL_Blue || Color1 == EColorLights::CL_Green;
+	bool isCyanB = Color2 == EColorLights::CL_Blue || Color2 == EColorLights::CL_Green;
+	bool isYellowA = Color1 == EColorLights::CL_Red || Color1 == EColorLights::CL_Green;
+	bool isYellowB = Color2 == EColorLights::CL_Red || Color2 == EColorLights::CL_Green;
+	bool isMagentaA = Color1 == EColorLights::CL_Blue || Color1 == EColorLights::CL_Red;
+	bool isMagentaB = Color2 == EColorLights::CL_Blue || Color2 == EColorLights::CL_Red;
+	if (Color1 != EColorLights::CL_Nothing && Color2 != EColorLights::CL_Nothing)
+	{
+		if (isCyanA && isCyanB)
+		{
+			return EColorLights::CL_Cyan;
+		}
+		if (isYellowA && isYellowB)
+		{
+			return EColorLights::CL_Yellow;
+		}
+		if (isMagentaA && isMagentaB)
+		{
+			return EColorLights::CL_Magenta;
+		}
+	}
+
+
+	return EColorLights::CL_Nothing;
+}
+
+void UEntityPhysicsModifier::ActivateLowGravity()
+{
+	MovementComponent->GravityScale = LowGravityScale;
+}
+
+void UEntityPhysicsModifier::ActivateFastAsFuckBoi()
+{
+	MovementComponent->MaxAcceleration = MaxAcceleration;
+}
+
+void UEntityPhysicsModifier::ActivateSlowMotion()
+{
+	MovementComponent->BrakingFriction = FrictionCoef;
+}
+
+void UEntityPhysicsModifier::ResetPhysics()
+{
+	MovementComponent->GravityScale = 1;
+	MovementComponent->MaxAcceleration = 600;
+	MovementComponent->BrakingFriction = 1;
+}
+
---
+++ Source/FPSGameJam2022/EntityPhysicsModifier.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EntityPhysicsModifier.generated.h"
+
+UENUM(BlueprintType)
+enum class EColorLights : uint8 {
+	CL_Red UMETA(DisplayName = "Red"), CL_Green UMETA(DisplayName = "Green"), CL_Blue UMETA(DisplayName = "Blue"),
+	CL_Cyan UMETA(DisplayName = "Cyan"), CL_Magenta UMETA(DisplayName = "Magenta"), CL_Yellow UMETA(DisplayName = "Yellow"), CL_Nothing UMETA(DisplayName = "Nothing")
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class FPSGAMEJAM2022_API UEntityPhysicsModifier : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UEntityPhysicsModifier();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY()
+	UCharacterMovementComponent* MovementComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = " Modifiers ")
+	float LowGravityScale;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = " Modifiers ")
+	float MaxAcceleration;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = " Modifiers ")
+	float FrictionCoef;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = " Color ")
+	EColorLights Color1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = " Color ")
+	EColorLights Color2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = " Color ")
+	EColorLights MixedColor;
+
+	UFUNCTION(BlueprintCallable)
+	void SetColor(EColorLights NewColor);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveColor(EColorLights ColorToRemove);
+
+	UFUNCTION(BlueprintCallable)
+	EColorLights GetMixedColorStage();
+
+	UFUNCTION(BlueprintCallable)
+	void ActivateLowGravity();
+
+	UFUNCTION(BlueprintCallable)
+	void ActivateFastAsFuckBoi();
+
+	UFUNCTION(BlueprintCallable)
+	void ActivateSlowMotion();
+
+	UFUNCTION(BlueprintCallable)
+	void ResetPhysics();
+		
+};
